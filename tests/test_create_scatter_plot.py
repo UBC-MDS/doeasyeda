dfdfdc92aa3c22bf9319a29f78e19e8882dff3b6
Dan Zhang <30704966+wqxxzd@users.noreply.github.com>
2024-01-31 10:48:31 -0800

    Update test_create_scatter_plot.py

--- tests/test_create_scatter_plot.py
+++ tests/test_create_scatter_plot.py
@@ -1,8 +1,9 @@
 import altair as alt
 import pandas as pd
 import pytest
+from unittest.mock import patch
 
-from doeasyeda.create_scatter_plot import create_scatter_plot
+from doeasyeda.create_scatter_plot import create_scatter_plot, DoEasyEDAException
 
 
 def test_scatter_plot_successful_plot_creation():
@@ -95,3 +96,68 @@ def test_scatter_plot_with_title():
 
     chart_dict = chart.to_dict()
     assert chart_dict['title'] == title
+
+
+def test_scatter_plot_with_color():
+    """
+    Test the scatter plot creation with color.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    df = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6], 'z': ['red', 'green', 'blue']})
+    chart = create_scatter_plot(df, 'x', 'y', color='z')
+
+    assert chart.encoding['color'].shorthand == 'z'
+
+
+def test_scatter_plot_with_tooltip():
+    """
+    Test the scatter plot creation with tooltip.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    df = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6], 'z': ['red', 'green', 'blue']})
+    chart = create_scatter_plot(df, 'x', 'y', tooltip=['z'])
+
+    assert chart.encoding['tooltip'][0].shorthand == 'z'
+
+
+def test_scatter_plot_with_linteractive():
+    """
+    Test the scatter plot creation with interactive.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    df = pd.DataFrame({'x': [1, 2, 3], 'y': [4, 5, 6]})
+    chart = create_scatter_plot(df, 'x', 'y', interactive=True)
+
+    assert chart.interactive
+
+
+def test_do_easy_eda_exception():
+    # Create a sample dataframe
+    df = pd.DataFrame({'x': [1, None, 3], 'y': [None, 5, 6]})
+
+    with patch('doeasyeda.create_scatter_plot.alt.Chart') as mock_chart:
+        mock_chart.side_effect = Exception("Test Exception")
+        
+        with pytest.raises(DoEasyEDAException):
+            create_scatter_plot(df, 'x', 'y')
